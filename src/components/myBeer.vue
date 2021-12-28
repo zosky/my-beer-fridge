cef2c82200fd32b78ca4097f9062ec2220014d52
marc bulbulian <boolioncube@gmail.com>
2021-12-28 15:18:00 -0500

    barView qty by fives

--- src/components/myBeer.vue
+++ src/components/myBeer.vue
@@ -8,10 +8,14 @@ defineProps({
 
 <template>
     <article>
-        <SvgIcons 
-            v-for="x in qty" :key="x" 
-            :icon="-1" class="qty" 
-        />
+        <template v-for="x in qty" :key="x" >
+            <SvgIcons 
+                v-if="x%5!=0" 
+                :icon="-1" 
+                :class="['qty',{more:x>5 && x%5==1}]" 
+            />
+            <div v-else class="strike" />
+        </template>
         <h3 v-text="beer" />
         <div>
         </div>
@@ -19,7 +23,9 @@ defineProps({
 </template>
 
 <style scoped>
-    article { @apply flex flex-row gap-x-1 items-center hover:text-blue-600 }
+    article { @apply flex flex-row gap-x-1 items-center hover:text-blue-600 relative }
     h3 { @apply font-bold text-sm }
     .qty { @apply h-4 w-auto -mx-0.5 }
+    .strike { @apply w-6 bg-blue-600 h-1 absolute -left-1 transform -rotate-6 rounded-full bg-opacity-75 }
+    .qty.more { @apply pl-1 }
 </style>
